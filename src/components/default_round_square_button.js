8614752166a7d6f169dc83be05e28a99dae55da4
b0rgon <alexandrebg07@gmail.com>
2025-05-25 16:50:57 +0100

    inicio da implementação da lógica de botão selecionado e alteração consequente da view

---
+++ src/components/default_round_square_button.js
@@ -0,0 +1,51 @@
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React from 'react'
+import Colors from '../../constants/colors'
+import Fonts from '../../constants/font_styles'
+
+const DefaultRoundSquareButton = ({ id, text, subtext, selectedBtn, setSelectedBtn }) => {
+    let isSelected = id === selectedBtn;
+
+    return (
+        <TouchableOpacity onPress={() => setSelectedBtn(id)}>
+            <View style={[styles.container,
+            isSelected && { backgroundColor: Colors.secondaryColorWithOpacity }
+            ]}>
+                <View style={styles.textArea}>
+                    <Text style={styles.text}>{text}</Text>
+                </View>
+                <Text style={styles.subtext}>{subtext}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        borderColor: Colors.secondaryColor,
+        borderWidth: 1,
+        borderRadius: 15,
+        paddingHorizontal: 10,
+        width: 100,
+        height: 130,
+        paddingBottom: 10,
+        paddingTop: 6,
+    },
+    textArea: {
+        height: 35,
+        justifyContent: 'center',
+        marginBottom: 6
+    },
+    text: {
+        color: Colors.mainTextColor,
+        fontFamily: Fonts.quicksandBold,
+        fontSize: 12
+    },
+    subtext: {
+        color: Colors.mainTextColor,
+        fontFamily: Fonts.quicksandRegular,
+        fontSize: 12,
+    }
+})
+
+export default DefaultRoundSquareButton
--- src/screens/new_workout.js
+++ src/screens/new_workout.js
@@ -1,13 +1,17 @@
 import { View, Text, StyleSheet, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import Colors from '../../constants/colors'
 import { LinearGradient } from 'expo-linear-gradient'
 import BackButton from '../components/back_button'
 import ConfirmButton from '../components/confirm_button'
 import Icon from 'react-native-vector-icons/AntDesign'
 import Fonts from '../../constants/font_styles'
+import DefaultRoundSquareButton from '../components/default_round_square_button'
 
 const NewWorkout = () => {
+
+    const [selectedBtn, setSelectedBtn] = useState('SCRATCH');
+
     return (
         <ScrollView contentContainerStyle={{ flexGrow: 1 }} keyboardShouldPersistTaps={'always'}>
             <LinearGradient style={styles.container} colors={Colors.backgroundColor}>
@@ -17,6 +21,33 @@ const NewWorkout = () => {
                         <Text style={styles.headerLabel}>New workout</Text>
                         <ConfirmButton />
                     </View>
+                    <Text style={styles.chooseMethodLabel}>Choose a method</Text>
+                    <View style={styles.methods}>
+                        <DefaultRoundSquareButton
+                            id='COPY'
+                            text='Copy last workout'
+                            subtext='No data found.' // o subtext aqui vão ser dados da bd
+                            selectedBtn={selectedBtn}
+                            setSelectedBtn={setSelectedBtn}
+                        />
+                        <DefaultRoundSquareButton
+                            id='CHOOSE'
+                            text='Choose from My Routines'
+                            subtext="I want to choose from the templates I've created"
+                            selectedBtn={selectedBtn}
+                            setSelectedBtn={setSelectedBtn}
+                        />
+                        <DefaultRoundSquareButton
+                            id='SCRATCH'
+                            text='Add as I go'
+                            subtext='I wanna start from scratch.'
+                            selectedBtn={selectedBtn}
+                            setSelectedBtn={setSelectedBtn}
+                        />
+                    </View>
+                    <Text style={styles.chooseMethodLabel}>{selectedBtn}</Text>
+
+
                 </View>
             </LinearGradient>
         </ScrollView>
@@ -31,7 +62,6 @@ const styles = StyleSheet.create({
     },
     root: {
         padding: 70,
-        alignItems: 'center',
     },
     header: {
         flexDirection: 'row',
@@ -44,7 +74,18 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontFamily: Fonts.quicksandBold,
     },
-
+    chooseMethodLabel: {
+        marginTop: 50,
+        color: Colors.secondaryTextColor,
+        fontFamily: Fonts.asapRegular,
+        fontSize: 18
+    },
+    methods: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginTop: 25
+    }
 })
 
 export default NewWorkout
